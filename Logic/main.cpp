1aa7960c2bd50db0b6601d5caff6c394969ce922
HoorayLee <leehorray@gmail.com>
2016-11-17 15:00:27 -0800

    Initial Commit

---
+++ Logic/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Logic
+//
+//  Created by 洪锐 李 on 11/17/16.
+//  Copyright © 2016 洪锐 李. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
